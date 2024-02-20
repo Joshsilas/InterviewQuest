021aca8417bc5b5b670d5a375df1efecc9b9033d
Joshsilas <137796554+Joshsilas@users.noreply.github.com>
2024-02-20 13:47:20 +0000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+        <h1>Job Hunt</h1>
+        <p>The Game</p>
+
+    </>
+  )
+}
+
+export default App
